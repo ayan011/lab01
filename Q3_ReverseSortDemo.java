60ead272f82a3fbe587e2ec64474de49f44680e1
ayan011 <35842863+ayan011@users.noreply.github.com>
2020-05-17 22:55:02 -0400

    Update Q3_ReverseSortDemo.java

--- Q3_ReverseSortDemo.java
+++ Q3_ReverseSortDemo.java
@@ -1,17 +1,28 @@
-public class Q3_ReverseSortDemo {
+public class Q3_ReverseSortDemo{
 
-  public static void main(String[] args){
-    char[] unorderedLetters;
-    unorderedLetters = new char[]{'b', 'm', 'z', 'a', 'u'};
-    reverseSort(unorderedLetters);
-    for (int i = 0 ; i < unorderedLetters.length; i++ )
-      System.out.print(unorderedLetters[i]);
-  }
+	public static void main(String[] args){
+		char[] unorderedLetters;
+		unorderedLetters = new char[]{'b','m','z','a','u'};
+		reverseSort(unorderedLetters);
+		for(int i = 0; i < unorderedLetters.length; i++)
+			System.out.println(unorderedLetters[i]);
+	}
 
-  //method that sorts a char array into its reverse alphabetical order
-  public static void reverseSort(char[] values){
+	
+	public static void reverseSort(char[] values){
 
-    //your code here
-  }
+		for(int i = 0; i < values.length - 1; i++){
+			int argMax = i;
+			for(int j = i + 1; j < values.length; j++){
+				
+				if(values[j]>values[argMax]) {
+					argMax = j;
+				}
+			}
 
+			char tmp = values[argMax];
+			values[argMax] = values[i];
+			values[i] = tmp;
+		}
+	}
 }
